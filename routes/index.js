af1142e149c23c2f3a767ffc0993a2c487342345
MBest24 <mbekoe24@gmail.com>
2020-04-14 19:38:44 -0400

    updated recipe img & details

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-const { Router } = require('express')
-const controllers = require('../controllers')
-const restrict = require('../helpers')
+const { Router } = require("express");
+const controllers = require("../controllers");
+const restrict = require("../helpers");
 
-const router = Router()
+const router = Router();
 
-router.get('/', (req, res) => res.send('This is the api root!'))
+router.get("/", (req, res) => res.send("This is the api root!"));
 
-router.post('/sign-up', controllers.signUp)
-router.post('/sign-in', controllers.signIn)
-router.get('/verify', controllers.verifyUser)
-router.post('/change-password', controllers.changePassword)
+router.post("/sign-up", controllers.signUp);
+router.post("/sign-in", controllers.signIn);
+router.get("/verify", controllers.verifyUser);
+router.post("/change-password", controllers.changePassword);
 
-router.get('/recipes', controllers.getRecipes)
-router.get('/recipes/:id', restrict, controllers.getRecipe)
-router.post('/recipes', restrict, controllers.createRecipe)
-router.put('/recipes/:id', restrict, controllers.updateRecipe)
-router.delete('/recipes/:id', restrict, controllers.deleteRecipe)
+router.get("/recipes", controllers.getRecipes);
+router.get("/recipes/:id", restrict, controllers.getRecipe);
+router.post("/recipes", restrict, controllers.createRecipe);
+router.put("/recipes/:id", restrict, controllers.updateRecipe);
+router.delete("/recipes/:id", restrict, controllers.deleteRecipe);
 
-module.exports = router
+module.exports = router;
