a967172c14a26f4614dbbd889991b9bd6cc940d9
Myra <myrakornides@gmail.com>
2020-04-16 10:26:23 -0400

    nav files align with what koran submited yesterday

--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -11,24 +11,24 @@ const authenticatedOptions = (
     <NavLink className="link" to="/add-recipe">
       Add Recipe
     </NavLink>
-                <NavLink className="link" to="/sign-out">
-                        Sign Out
+    <NavLink className="link" to="/sign-out">
+      Sign Out
     </NavLink>
-        </>
+  </>
 );
 
 const unauthenticatedOptions = (
-        <>
-                <NavLink className="link" to="/sign-up">
-                        Sign Up
+  <>
+    <NavLink className="link" to="/sign-up">
+      Sign Up
     </NavLink>
-                <NavLink className="link" to="/sign-in">
-                        Sign In
+    <NavLink className="link" to="/sign-in">
+      Sign In
     </NavLink>
-        </>
+  </>
 );
 
- 
+
 // const alwaysOptions = (
 //   <>
 //   </>
@@ -38,10 +38,12 @@ const Nav = ({ user }) => {
   return (
     <nav>
       <div className="nav">
-        <NavLink className="logo" to="/">
-          My Recipe Book
-        </NavLink>
-            <img src={logo} alt="logo" />
+        <div className="logo">
+          <NavLink className="heading" to="/">
+            My Recipe Book
+          </NavLink>
+          <img src='https://i.imgur.com/yGz0wMf.png' alt="salad bowl" />
+        </div>
         <div className="links">
           {/* {user && <div className="link welcome">Welcome, {user.username}</div>} */}
           {/* {alwaysOptions} */}
