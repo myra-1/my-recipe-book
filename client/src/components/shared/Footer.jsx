671b0218d29cdab38bfecefedb26b5cfd65d540c
Koran Agan <koranagan@gmail.com>
2020-04-16 09:24:08 -0400

    logo, nav and footer style

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () =>
-    <footer></footer>
+        <footer>
+
+
+
+                <div className="copyright">
+                        <h2><span>Tiny</span> <span>Little</span> <span>Features</span> <span>&copy;</span> <span>2020</span></h2>
+                </div>
+
+
+
+
+        </footer>
 
 export default Footer
--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -1,52 +1,57 @@
 import React from "react";
 import "./Nav.css";
 import { NavLink } from "react-router-dom";
+import logo from './logo.png';
 
 const authenticatedOptions = (
-  <>
-    <NavLink className="link" to="/add-recipe">
-      Add Recipe
+        <>
+                <NavLink className="link" to="/add-recipe">
+                        Add Recipe
     </NavLink>
-    <NavLink className="link" to="/sign-out">
-      Sign Out
+                <NavLink className="link" to="/sign-out">
+                        Sign Out
     </NavLink>
-  </>
+        </>
 );
 
 const unauthenticatedOptions = (
-  <>
-    <NavLink className="link" to="/sign-up">
-      Sign Up
+        <>
+                <NavLink className="link" to="/sign-up">
+                        Sign Up
     </NavLink>
-    <NavLink className="link" to="/sign-in">
-      Sign In
+                <NavLink className="link" to="/sign-in">
+                        Sign In
     </NavLink>
-  </>
+        </>
 );
 
 const alwaysOptions = (
-  <>
-    <NavLink className="link" to="/recipes">
-      Recipes
+        <>
+                <NavLink className="link" to="/recipes">
+                        Recipes
     </NavLink>
-  </>
+        </>
 );
 
 const Nav = ({ user }) => {
-  return (
-    <nav>
-      <div className="nav">
-        <NavLink className="logo" to="/">
-          My Recipe Book
-        </NavLink>
-        <div className="links">
-          {user && <div className="link welcome">Welcome, {user.username}</div>}
-          {alwaysOptions}
-          {user ? authenticatedOptions : unauthenticatedOptions}
-        </div>
-      </div>
-    </nav>
-  );
+        return (
+                <nav>
+                        <div className="nav">
+                                <div className="logo">
+                                        <NavLink className="heading" to="/">
+                                                My Recipe Book
+                                </NavLink>
+                                        <img src={logo} alt="logo" />
+                                </div>
+                                <div className="links">
+                                        {user && <div className="link welcome">Welcome, {user.username}</div>}
+                                        {alwaysOptions}
+                                        {user ? authenticatedOptions : unauthenticatedOptions}
+                                </div>
+                        </div>
+                        <hr></hr>
+                </nav>
+        );
 };
 
 export default Nav;
