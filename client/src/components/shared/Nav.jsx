4f43b2efccdf6aa4220253a92c7ae895b4aea52f
MBest24 <mbekoe24@gmail.com>
2020-04-16 10:41:28 -0400

    edited nav

--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Nav.css";
 import { NavLink } from "react-router-dom";
-import logo from './logo.png';
-
 const authenticatedOptions = (
   <>
     <NavLink className="link" to="/recipes">
@@ -11,37 +9,35 @@ const authenticatedOptions = (
     <NavLink className="link" to="/add-recipe">
       Add Recipe
     </NavLink>
-                <NavLink className="link" to="/sign-out">
-                        Sign Out
+    <NavLink className="link" to="/sign-out">
+      Sign Out
     </NavLink>
-        </>
+  </>
 );
-
 const unauthenticatedOptions = (
-        <>
-                <NavLink className="link" to="/sign-up">
-                        Sign Up
+  <>
+    <NavLink className="link" to="/sign-up">
+      Sign Up
     </NavLink>
-                <NavLink className="link" to="/sign-in">
-                        Sign In
+    <NavLink className="link" to="/sign-in">
+      Sign In
     </NavLink>
-        </>
+  </>
 );
-
- 
 // const alwaysOptions = (
 //   <>
 //   </>
 // );
-
 const Nav = ({ user }) => {
   return (
     <nav>
       <div className="nav">
-        <NavLink className="logo" to="/">
-          My Recipe Book
-        </NavLink>
-            <img src={logo} alt="logo" />
+        <div className="logo">
+          <NavLink className="heading" to="/">
+            My Recipe Book
+          </NavLink>
+          <img src="https://i.imgur.com/yGz0wMf.png" alt="salad bowl" />
+        </div>
         <div className="links">
           {/* {user && <div className="link welcome">Welcome, {user.username}</div>} */}
           {/* {alwaysOptions} */}
@@ -52,5 +48,4 @@ const Nav = ({ user }) => {
     </nav>
   );
 };
-
 export default Nav;
